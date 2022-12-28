041f2d955d9f6c1306ef59d3b87dc9f67718e9c9
Frankie Xiao <fxiaoX089@live.com>
2022-12-27 18:01:41 -0600

    add input validation

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,31 @@
 import "../style.css"
+
+const formInputs = document.querySelectorAll("form input")
+const formSelect = document.querySelector("select")
+
+function inputValidation() {
+    formInputs.forEach((input) => {
+        input.onblur = (e) => {
+            if (!e.target.value) {
+                e.target.style.border = "2px solid red"
+            } else {
+                e.target.style.border = "none"
+            }
+        }
+    })
+}
+
+function stateValidation() {
+    formSelect.onblur = (e) => {
+        if (!formSelect.value) {
+            formSelect.style.color = "red"
+        }
+    }
+}
+
+function formValidation() {
+    inputValidation()
+    stateValidation()
+}
+
+formValidation()
